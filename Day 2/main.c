94715dbf6b0499a7ec2c6844dc26609d640cde8a
Skotuson <58478268+Skotuson@users.noreply.github.com>
2021-12-02 19:07:48 +0100

    d2: complete solution for day 2

--- Day 2/main.c
+++ Day 2/main.c
@@ -3,14 +3,16 @@
 
 int main ( void ) {
 	char mov[8];
-	int n, d = 0, f = 0;
+	int n, aim = 0, d = 0, f = 0;
 	while ( scanf ( "%s %d", mov, &n ) == 2 ) {
-		 if ( !strcmp ( "forward", mov ) )
-		 	f += n;   
+		 if ( !strcmp ( "forward", mov ) ) {
+		 	f += n;
+		 	d += aim * n; 
+		 } 
 		 else if ( !strcmp ( "down", mov ) )
-		 	d += n;
+		 	aim += n;
 		 else if ( !strcmp ( "up", mov ) ) 
-		 	d -= n;
+		 	aim -= n;
 	} 
 	printf ( "Res: %d\n", d * f ) ;
 	return 0;
