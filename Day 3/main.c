f2ff961294dda3c87da6c5706dd57eb7b894aefe
Skotuson <58478268+Skotuson@users.noreply.github.com>
2021-12-03 15:03:51 +0100

    d3: almost complete solution

--- Day 3/main.c
+++ Day 3/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main ( void ) {
+#define BIN_LEN 12
 
+int main ( void ) {
+    int gamma = 0, epsilon = 0;
+    int digits [BIN_LEN] = {};
+    char c; 
+    int i = 0;
+    
+    while ( scanf ( "%c", &c ) == 1 ) {
+        if ( c == '\n' ) continue;
+        if ( c - '0' ) digits[ i ]++;
+        else digits[ i ]--;
+        if ( ++i == 12 )
+            i = 0;
+    }
+
+    for ( int i = 0; i < BIN_LEN; i++ ) {
+        if ( digits[ i ] > 0 ) digits [ i ] = 1;
+        else digits [ i ] = 0;
+        gamma += digits[ i ] * 2;
+        epsilon += !digits[ i ] * 2;
+    }
+    printf( "Power consumption: %d\n", gamma * epsilon );
     return 0;
 }
